a7d08331ab9ef42238f111e29c0abdb7dfcc2fd7
ChilliCat <36356602+JoeCat1207@users.noreply.github.com>
2025-02-19 13:10:23 -0500

    new code

---
+++ rfidproject.ino
@@ -0,0 +1,20 @@
+const int rst = 9;
+const int miso = 12;
+const int mosi = 11;
+const int sck = 13;
+const int sda = 10;
+const int LEDred = 2;
+const int LEDgreen = 4;
+const int LEDyellow = 6;
+#include <SPI.h>
+#include <MFRC522.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
